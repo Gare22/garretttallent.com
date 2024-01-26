77c8806149218a1c75850503011baf56c5232476
Garrett Tallent <garretttallent@gmail.com>
2024-01-26 14:10:35 -0500

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GarrettTallent.com</title>
+</head>
+<body>
+    <?php 
+    //Portfolio website
+    ///Each role I fill will have a subsection
+    ////each role will have related skills
+    /////each skill will have related projects
+    //Roles:
+    ///Web Dev
+    ///Game Dev
+    ///CAD Designer/Technician
+    ///Game Host
+        echo"<h1>I'm Garrett Tallent</h1>";
+    ?>
+</body>
+</html>
